3841555c3fb67b52b0bba176e7d4a908b5b545bf
Hirushiharan <hirushiharant@gmail.com>
2024-09-01 12:31:52 +0530

    feat: configure env variables

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Load environment variables
 MSSQL_SERVER = os.getenv('MSSQL_SERVER')
@@ -9,3 +12,7 @@ MSSQL_DB = os.getenv('MSSQL_DB')
 
 # Construct the DATABASE_URL
 DATABASE_URL = f"sqlserver://{MSSQL_USER}:{MSSQL_PASSWORD}@{MSSQL_SERVER}:{MSSQL_PORT}/{MSSQL_DB}"
+
+# Secret key for JWT encoding and decoding
+SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
